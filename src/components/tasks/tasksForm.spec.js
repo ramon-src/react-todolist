90a841b394ac7f6c132706821dd55c2341f201ec
Ramon Schmidt <ramon@trin.ca>
2020-01-29 09:18:15 -0300

    testing tasks form

--- src/components/tasks/tasksForm.spec.js
+++ src/components/tasks/tasksForm.spec.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
+import userEvent from "@testing-library/user-event";
 
 import TasksForm from "./tasksForm";
 
 describe("TaskForm", () => {
   describe("Estado inicial do Form", () => {
-   
     it("deve mostrar todos os campos ao iniciar form", () => {
       const props = {
         task: {
@@ -28,10 +28,27 @@ describe("TaskForm", () => {
     });
 
     it("deve esconder modal ao iniciar o form", () => {});
-
   });
-  
+
   describe("Contexto de envio do form", () => {
-    it("deve mostrar modal quando dados válidos forem submetidos", () => {});
+    it("deve mostrar modal e mensagem Fill in all the fields quando form estiver vazio", () => {
+      const props = {
+        task: {
+          name: "",
+          type: "",
+          day: "",
+          time: "",
+          createdAt: ""
+        }
+      };
+      const { getByTestId, getByText, getByPlaceholderText } = render(
+        <TasksForm {...props} />
+      );
+
+      const button = getByText("Submit");
+      userEvent.click(button);
+      const modal = getByText("Fill in all the fields");
+      expect(modal).toBeVisible();
+    });
   });
 });
